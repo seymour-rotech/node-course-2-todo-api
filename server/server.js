1b942620d180967d910c00fd89599e8b1988bf9b
Seymour <seymour@rotech.nz>
2018-06-22 01:07:40 +1200

    Add Get /todos/:id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser')
 var {mongoose} = require('./db/mongoose')
 var {Todo} = require('./models/todo')
 var {User} = require('./models/user')
+var {ObjectID} = require('mongodb')
 
 var app = express()
 
@@ -26,6 +27,35 @@ app.get('/todos',
         Todo.find().then(
             (todos) => { res.send({todos}) },
             (e) => {res.status(400).send(e)}
+        )
+    }
+)
+
+app.get('/todos/:id', 
+    (req, res) =>{
+        //res.send(req.params) 
+        var id = req.params.id;
+
+        if (!ObjectID.isValid(id)) {
+            return res.status(404).send("Id is invalid")
+        }
+
+        Todo.findById(id).then(
+            (todo) => {
+                if (!todo)
+                    {
+                        return res.send('Unable to find Todo')
+                    }
+
+                res.send(JSON.stringify({todo}, undefined,2))
+                },
+            (err) => {
+                res.status(404).send(err)
+            }
+        ).catch(
+            (err) => {
+                res.status(404).send(err)
+            }
         )
     }
 )
