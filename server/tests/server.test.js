1597eda54a7a64987e9966c6ebd00ccbb5ce7e63
Seymour <seymour@rotech.nz>
2018-06-25 01:07:04 +1200

    Test DELETE /todos/:id route'

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -133,3 +133,50 @@ describe ('GET /todos/:id',
 
     }
 )
+
+describe ('DELETE /todos/:id', () => {
+
+     it ('should remove a todo', (done) => {
+        var hexId = todos[1]._id.toHexString()
+
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo._id).toBe(hexId);
+                })
+            .end ( (err, res) => {
+                    if (err) {
+                        return done(err)
+                    }
+                
+                Todo.findById(hexId).then(
+                    (todo) => {
+                        expect(todo).toNotExist()
+                        done()
+                    }
+                ).catch(
+                    (e) => { done(e) }
+                )
+            })
+    })
+
+    it ('shoudd return 404 if todo not found', 
+        (done) => {
+            request(app)
+            .delete(`/todos/${(new ObjectID()).toHexString()}`)
+            .expect(404)
+            .end(done)
+        }
+    )
+
+    it('should return 404 if non-object id', 
+        (done) => {
+            request(app)
+            .delete('/todo/31234132')
+            .expect(404)
+            .end(done)
+        }
+    )
+
+})
